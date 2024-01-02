b937156a4a034b470ed7bc1e50a41bc6bc47ab66
kalifun <kalifun@163.com>
2024-01-02 15:30:05 +0800

    modify nvim-treesitter options

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -41,4 +41,28 @@ return {
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "comment",
+          "diff",
+          "dockerfile",
+          "dot",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "graphql",
+          "hcl",
+          "http",
+          "jq",
+          "make",
+          "mermaid",
+          "sql",
+        })
+      end
+    end,
+  },
 }
