6060df5e2ea9717d6d6442cb77fd1bb7710235ff
kalifun <kalifun@163.com>
2024-01-02 15:44:01 +0800

    modify neo-tree options

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
+      },
+    },
+  },
+}
