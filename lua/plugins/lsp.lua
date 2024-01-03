b4ef37d9d6e119fd30a9a6104c61d3c13fde1fe7
kalifun <kalifun@163.com>
2024-01-03 09:35:59 +0800

    add nvim-dap options

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,4 +11,43 @@ return {
       }
     end,
   },
+  -- dap integration
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    keys = {
+      {
+        "<leader>de",
+        function()
+          require("dap.ui.widgets").centered_float(require("dap.ui.widgets").expression, { border = "none" })
+        end,
+        desc = "Eval",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>dwf",
+        function()
+          require("dap.ui.widgets").centered_float(require("dap.ui.widgets").frames, { border = "none" })
+        end,
+        desc = "Frames",
+      },
+      {
+        "<leader>dws",
+        function()
+          require("dap.ui.widgets").centered_float(require("dap.ui.widgets").scopes, { border = "none" })
+        end,
+        desc = "Scopes",
+      },
+      {
+        "<leader>dwt",
+        function()
+          require("dap.ui.widgets").centered_float(require("dap.ui.widgets").threads, { border = "none" })
+        end,
+        desc = "Threads",
+      },
+    },
+    opts = function(_, opts)
+      require("dap").defaults.fallback.terminal_win_cmd = "enew | set filetype=dap-terminal"
+    end,
+  },
 }
