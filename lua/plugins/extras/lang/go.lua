a804c3c34edcc06422b1b46a8330099aeffd5127
kalifun <kalifun@163.com>
2024-02-27 11:40:11 +0800

    feat: add go plugin

---
+++ lua/plugins/extras/lang/go.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "olexsmir/gopher.nvim",
+    requires = { -- dependencies
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    keys = {
+      {
+        "<leader>gsj",
+        "<cmd> GoTagAdd json <CR>",
+        "Add json struct tags",
+      },
+      {
+        "<leader>gsy",
+        "<cmd> GoTagAdd yaml <CR>",
+        "Add yaml struct tags",
+      },
+      {
+        "<leader>gmt",
+        "<cmd> GoMod tidy <CR>",
+        "Go mod tidy",
+      },
+      {
+        "<leader>ggc",
+        "<cmd> GoGenerate % <CR>",
+        "Go generate for current file",
+      },
+      {
+        "<leader>gtf",
+        "<cmd> GoTestsAll <CR>",
+        "Go generate all tests for all function/methods in current file",
+      },
+      {
+        "<leader>gto",
+        "<cmd> GoTestAdd <CR>",
+        "Go generate ont test for a specific function/methods",
+      },
+      {
+        "<leader>gie",
+        "<cmd> GoIfErr <CR>",
+        "Set cursor on the line with err",
+      },
+    },
+    build = function()
+      vim.cmd([[silent! GoInstallDeps]])
+    end,
+  },
+}
